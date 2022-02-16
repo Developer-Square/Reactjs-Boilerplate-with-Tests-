f3f8dc102b3994e273da9a83a40369843f69ffdb
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-16 15:17:24 +0300

    feat: Added test to make ci fail

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Apps />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn reacts/i);
   expect(linkElement).toBeInTheDocument();
 });
